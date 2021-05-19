5d1c0fba4483a64588b6329a95458bbbb22d220b
David Dudek <81631429+captivea-ddu@users.noreply.github.com>
2021-05-19 13:25:19 -0400

    comment out

--- cAP-tets/trial_for_tracking/__manifest__.py
+++ cAP-tets/trial_for_tracking/__manifest__.py
@@ -24,7 +24,7 @@
         # "views/medquest_home.xml",
         # "views/service_details.xml",
         # "views/service_general.xml",
-        'views/test.xml',
+        # 'views/test.xml',
         ],
     "images": [],
     "application": True,
