54cc817b37456c312922303f5ef79a9963d3348a
David Dudek <81631429+captivea-ddu@users.noreply.github.com>
2021-05-19 18:16:49 -0400

    created route for case_details

--- cAP-tets/trial_for_tracking/controllers/main.py
+++ cAP-tets/trial_for_tracking/controllers/main.py
@@ -34,24 +34,72 @@ class WebsiteSaleCustom(http.Controller):
 
 	@http.route('/submit/case-details/', type='http', auth='public', website=True, method='POST')
 	def other_form(self, **kw):
-		for key, value in kw.items():
-			print(key,value)
-
 		user_id = request.env.context.get('uid')
 		existing_details = request.env['service.request'].sudo().search([('user_id', '=', user_id)], limit=1)
-		if existing_details:
-			test = 1
-			#  Update
-		else:
-			test = 2
-			# Create
+		case_type = None
+		case_issues = None
+		case_overview = None
+		document_name = None
+		documents = None
 
-		print("$$$$$$$$$$$$$$$$ form submitted $$$$$$$$$$$$$$$$$",kw)
-		_logger.warning("$$$$$$$$$$$$$$$$ form submitted $$$$$$$$$$$$$$$$$\n" + str(kw))
 		for key, value in kw.items():
-			print(key,value)
-			_logger.warning(str(key) + " - " + str(value) + "\n")
-		return  request.render('website.layout')
+			# contact_name
+
+			if key == "case_type":
+				case_type = value
+			elif key == "case_overview":
+				case_overview = value
+			elif key == "case_issues":
+				case_issues = value
+			elif key == "document_name":
+				document_name = value
+			elif key == "documents"
+				documents = value
+		
+		if existing_details:
+			#  Update
+			existing_details.sudo().write({
+				'case_type': case_type,
+				'case_overview': case_overview,
+				'case_issues' : case_issues,
+			})
+			i = 1
+			for d in documents:
+				request.env['ir.attachment'].sudo().create({
+					'name': str(document_name) + "_" + str(i),
+					'type': 'binary',
+					'datas': d,
+					'res_model': 'service.request',
+					'res_id': existing_details.id,
+				})
+				i += 1
+
+		else:
+			# Create
+			service_request = request.env['service.request'].sudo().create({
+				'user_id': user_id,
+				'case_type': case_type,
+				'case_overview': case_overview,
+				'case_issues' : case_issues,
+			})
+			request.env.cr.commit()
+			i = 1
+			for d in documents:
+				request.env['ir.attachment'].sudo().create({
+					'name': str(document_name) + "_" + str(i),
+					'type': 'binary',
+					'datas': d,
+					'res_model': 'service.request',
+					'res_id': service_request.id,
+				})
+				i += 1
+
+		# print("$$$$$$$$$$$$$$$$ form submitted $$$$$$$$$$$$$$$$$",kw)
+		# _logger.warning("$$$$$$$$$$$$$$$$ form submitted $$$$$$$$$$$$$$$$$\n" + str(kw))
+		# for key, value in kw.items():
+		# 	print(key,value)
+		# 	_logger.warning(str(key) + " - " + str(value) + "\n")
+		return  request.redirect('/service-details')
 
 	@http.route('/', type='http', auth='public', website=True, method='GET')
 	def formsubmit(self, **kw):
--- cAP-tets/trial_for_tracking/models/website_menu.py
+++ cAP-tets/trial_for_tracking/models/website_menu.py
@@ -1,5 +1,9 @@
 from odoo import api, models, fields, _
 
+class Attachments(models.Model):
+    _inherit = 'ir.attachment'
+
+    service_request_id = fields.Many2one('service.request', string='Service Request Form')
 
 class SaleOrder(models.Model):
     _inherit = "website.menu"
@@ -16,13 +20,19 @@ class ServiceRequest(models.Model):
         string='User',
         help='User',
     )
-    # name = fields.Char(String="Name")
-    # name = fields.Char(String="Name")
-    # name = fields.Char(String="Name")
-    # name = fields.Char(String="Name")
-    # name = fields.Char(String="Name")
-    # name = fields.Char(String="Name")
-    # name = fields.Char(String="Name")
-    # name = fields.Char(String="Name")
-    # name = fields.Char(String="Name")
-    # name = fields.Char(String="Name")
+
+    case_type = fields.Selection([( 'motor','Motor Vehicle Accident'), 
+                                  ('nurse' , 'Nursing Home'),
+                                  ('defective','Defective Product'),
+                                  ('wrong','Wrongful Death'),
+                                  ('mal' ,'Medical Malpractice and Premises')])
+
+    case_overview = fields.Text(string='Overview')
+
+    case_issues = fields.Text(string='Case Issues and Focus')
+
+    documents = fields.One2many('ir.attachment', 'service_request_id', string="Attachments")
+
+    # contact_name = fields.Char(string="Contact Name")
+
+    # document_name = fields.Text()
