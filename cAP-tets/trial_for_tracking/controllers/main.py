61f39d8e1f0f50ac9776d855b981a30b234e735f
David Dudek <81631429+captivea-ddu@users.noreply.github.com>
2021-05-19 22:46:07 -0400

    Update main.py

--- cAP-tets/trial_for_tracking/controllers/main.py
+++ cAP-tets/trial_for_tracking/controllers/main.py
@@ -107,7 +107,7 @@ class WebsiteSaleCustom(http.Controller):
 		return  request.redirect('/service-details')
 
 
-@http.route('/service-details/cancel', type='http', auth='public', website=True, method='POST')
+	@http.route('/service-details/cancel', type='http', auth='public', website=True, method='POST')
 	def other_form(self, **kw):
 		user_id = request.env.context.get('uid')
 		existing_details = request.env['service.request'].sudo().search([('user_id', '=', user_id)], limit=1)
