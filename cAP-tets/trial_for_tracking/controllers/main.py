d202ca8857dde765bb547d316a23a316852c9d4a
David Dudek <81631429+captivea-ddu@users.noreply.github.com>
2021-05-20 22:31:26 -0400

    Update main.py

--- cAP-tets/trial_for_tracking/controllers/main.py
+++ cAP-tets/trial_for_tracking/controllers/main.py
@@ -8,7 +8,7 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 _logger = logging.getLogger(__name__)
 
 class WebsiteSaleCustom(http.Controller):
-
+	
 	@http.route('/service/', type='http', auth='public', website=True, method='GET')
 	def formsubmit(self, **kw):
 		print("########### Form Submitted ###########")
@@ -255,78 +255,78 @@ class WebsiteSaleCustom(http.Controller):
 			request.env.cr.commit()
 		return request.render('website.layout')
 
-	@http.route('/submit/case-details/', type='http', auth='public', website=True, method='POST')
-	def other_form(self, **kw):
-		_logger.warning("BIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIITCH")
-		return request.render("website.service_details")
-
 	# @http.route('/submit/case-details/', type='http', auth='public', website=True, method='POST')
 	# def other_form(self, **kw):
-	# 	_logger.warning("FUUUUUUUUUUCK YOUUUUUU ODOOOOOOO YOU MOTHERFUCKING PIECE OF SHIT SOFTWARE")
-	# 	user_id = request.env.context.get('uid')
-	# 	existing_details = request.env['service.request'].sudo().search([('user_id', '=', user_id)], limit=1)
-	# 	case_type = None
-	# 	case_issues = None
-	# 	case_overview = None
-	# 	document_name = None
-	# 	documents = None
-
-	# 	for key, value in kw.items():
-	# 		# contact_name
-
-	# 		if key == "case_type":
-	# 			case_type = value
-	# 		elif key == "case_overview":
-	# 			case_overview = value
-	# 		elif key == "case_issues":
-	# 			case_issues = value
-	# 		elif key == "document_name":
-	# 			document_name = value
-	# 		elif key == "documents":
-	# 			documents = value
-		
-	# 	if existing_details:
-	# 		#  Update
-	# 		existing_details.sudo().write({
-	# 			'case_type': case_type,
-	# 			'case_overview': case_overview,
-	# 			'case_issues' : case_issues,
-	# 		})
-	# 		i = 1
-	# 		if documents:
-	# 			for d in documents:
-	# 				request.env['ir.attachment'].sudo().create({
-	# 					'name': str(document_name) + "_" + str(i),
-	# 					'type': 'binary',
-	# 					'datas': d,
-	# 					'res_model': 'service.request',
-	# 					'res_id': existing_details.id,
-	# 				})
-	# 				i += 1
-
-	# 	else:
-	# 		# Create
-	# 		service_request = request.env['service.request'].sudo().create({
-	# 			'user_id': user_id,
-	# 			'case_type': case_type,
-	# 			'case_overview': case_overview,
-	# 			'case_issues' : case_issues,
-	# 		})
-	# 		request.env.cr.commit()
-	# 		i = 1
-	# 		if documents:
-	# 			for d in documents:
-	# 				request.env['ir.attachment'].sudo().create({
-	# 					'name': str(document_name) + "_" + str(i),
-	# 					'type': 'binary',
-	# 					'datas': d,
-	# 					'res_model': 'service.request',
-	# 					'res_id': service_request.id,
-	# 				})
-	# 				i += 1
-
-
+	# 	_logger.warning("BIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIITCH")
 	# 	return request.render("website.service_details")
+
+	@http.route('/submit/case-details/', type='http', auth='public', website=True, method='POST')
+	def case_details_form_controller(self, **kw):
+		_logger.warning("FUUUUUUUUUUCK YOUUUUUU ODOOOOOOO YOU MOTHERFUCKING PIECE OF SHIT SOFTWARE")
+		user_id = request.env.context.get('uid')
+		existing_details = request.env['service.request'].sudo().search([('user_id', '=', user_id)], limit=1)
+		case_type = None
+		case_issues = None
+		case_overview = None
+		document_name = None
+		documents = None
+
+		for key, value in kw.items():
+			# contact_name
+
+			if key == "case_type":
+				case_type = value
+			elif key == "case_overview":
+				case_overview = value
+			elif key == "case_issues":
+				case_issues = value
+			elif key == "document_name":
+				document_name = value
+			elif key == "documents":
+				documents = value
+		
+		if existing_details:
+			#  Update
+			existing_details.sudo().write({
+				'case_type': case_type,
+				'case_overview': case_overview,
+				'case_issues' : case_issues,
+			})
+			i = 1
+			if documents:
+				for d in documents:
+					request.env['ir.attachment'].sudo().create({
+						'name': str(document_name) + "_" + str(i),
+						'type': 'binary',
+						'datas': d,
+						'res_model': 'service.request',
+						'res_id': existing_details.id,
+					})
+					i += 1
+
+		else:
+			# Create
+			service_request = request.env['service.request'].sudo().create({
+				'user_id': user_id,
+				'case_type': case_type,
+				'case_overview': case_overview,
+				'case_issues' : case_issues,
+			})
+			request.env.cr.commit()
+			i = 1
+			if documents:
+				for d in documents:
+					request.env['ir.attachment'].sudo().create({
+						'name': str(document_name) + "_" + str(i),
+						'type': 'binary',
+						'datas': d,
+						'res_model': 'service.request',
+						'res_id': service_request.id,
+					})
+					i += 1
+
+
+		return request.render("website.service_details")
 
 
 	@http.route('/service-details/cancel', type='http', auth='public', website=False, method='POST')
