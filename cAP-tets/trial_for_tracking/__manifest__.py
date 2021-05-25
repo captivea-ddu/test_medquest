612f22113bb6366fca89255521557aa3434bcb14
David Dudek <81631429+captivea-ddu@users.noreply.github.com>
2021-05-25 09:46:41 -0400

    del

--- cAP-tets/trial_for_tracking/__manifest__.py
+++ cAP-tets/trial_for_tracking/__manifest__.py
@@ -25,7 +25,6 @@
         "views/service_details.xml",
         "views/service_general.xml",
         'views/header.xml',
-        'views/overlaytemplate.xml',
         ],
     "images": ["static/src/img/service_form.PNG"],
     "application": True,
